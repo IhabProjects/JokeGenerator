dd4edd52bd75c9db6caa65d100d61a0aea92656d
IhabProjects <eureka.lucis@gmail.com>
2025-03-20 22:42:34 +0000

    added fade transition

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,politic
 
 
 let getJoke = () => {
+    jokeContainer.classList.remove("fade");
     fetch(url)
     .then(data => data.json())
     .then(item => {
         jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+
     });
 }
 btn.addEventListener("click", getJoke)
